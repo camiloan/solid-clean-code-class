3495b40f8618fd4adfbfa5d1de08897d4a9b52dd
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-05 23:31:01 -0500

    refactor: improve product validation logic and enhance toString method

--- clean-course-main/src/clean-code/05-dry.ts
+++ clean-course-main/src/clean-code/05-dry.ts
@@ -10,13 +10,28 @@ class Product {
     ) {
     }
 
-    toString() {
-        // No Dry
-        if (this.name.length <= 0) throw Error('Name is empty');
-        if (this.price <= 0) throw Error('Price is zero');
-        if (this.size.length <= 0) throw Error('Size is empty');
+    isProductReady(): boolean {
+        
+        for( const key in this ) {
+            switch( typeof this[key] ) {
+                case 'string':
+                    if ( (<string><unknown>this[key]).length <= 0 ) throw Error(`${ key } is empty`);
+                break;
+                case 'number':
+                    if ( (<number><unknown>this[key]) <= 0 ) throw Error(`${ key } is zero`);
+                break;
+                default:
+                    throw Error(`${ typeof this[key] } is not valid`);
+            }
+        }
 
-        return `${this.name} (${this.price}) ,${this.size}`;
+        return true;
+    }
+
+    toString() {
+        if ( !this.isProductReady ) return;
+    
+        return `${ this.name } (${ this.price }), ${ this.size }`
     }
 }
 
