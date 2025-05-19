c98a10270773935d9b22d194228c4aca6eb92296
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-18 22:12:22 -0500

    feat: implement User and UserSettings classes with credential validation

--- clean-course-main/src/clean-code/06-classes-a.ts
+++ clean-course-main/src/clean-code/06-classes-a.ts
@@ -1,4 +1,7 @@
 (() => {
+
+    // No aplicando el princio de responsabilidad única
+
     type Gender = 'M' | 'F' | 'NB';
     class Person {
         constructor(
@@ -8,5 +11,41 @@
     }
 
     const newPerson = new Person('Fernando', 'M', new Date('1993-08-23'));
-    console.log({ newPerson });
+
+
+    class User extends Person {
+        public lastAccess: Date;
+        constructor(
+            public username: string,
+            public email: string,
+            public password: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials(password: string) {
+            return password === this.password;
+        }
+    }
+
+    const newUser = new User('fernando', 'fernando@gmail.com', '123456', 'Fernando', 'M', new Date('1993-08-23'));
+
+    class UserSettings extends User {
+        constructor(public workingDirectory: string,
+            public lastOpenFolder: string,
+            username: string,
+            email: string,
+            password: string,
+            name: string, gender: Gender, birthdate: Date) {
+            super(username, email, password, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings('C:\\Users\\fernando', 'C:\\Users\\fernando', 'fernando', 'fernando@gmail.com', '123456', 'Fernando', 'M', new Date('1993-08-23'));
+
+    console.log({ userSettings, areCredetentialsValid: userSettings.checkCredentials('123456') });
 })
